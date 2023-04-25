9cf3c01bb057260265efa5bcfe302e086990ddb2
Lutfunnahar Lota <l.nahar@ppln.co>
2023-04-25 13:55:19 +0600

    updated

--- main.py
+++ main.py
@@ -7,9 +7,13 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {'data':{'name':'Mst Lutfunnahar Lota'}}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get('/about')
+def about():
+    return {'data': {'about pages for information'}}
